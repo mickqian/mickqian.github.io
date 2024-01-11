42664a4b09725508dbb3832a332bb8fe86d6d133
Mick <mickjagger19@icloud.com>
2024-01-11 21:27:21 +0800

    new

--- public/posts/music/test.js
+++
@@ -1,9 +0,0 @@
-
-
-import {opensheetmusicdisplay} from "../../../static/js/opensheetmusicdisplay";
-
-var osmd = new opensheetmusicdisplay.OpenSheetMusicDisplay("osmd-container");
-osmd.load("/MusicXML/Ballade.musicxml").then(function() {
-    osmd.leadsheet();
-    osmd.render();
-});
